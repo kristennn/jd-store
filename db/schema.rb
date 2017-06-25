b93ded40f1a5211572ce4041e8bb45edb8420880
13949836783@163.com <13949836783@163.com>
2017-06-25 12:15:02 +0800

    admin can uploader image for product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625031958) do
+ActiveRecord::Schema.define(version: 20170625034749) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170625031958) do
     t.datetime "updated_at",  null: false
     t.integer  "price"
     t.integer  "quantity"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
